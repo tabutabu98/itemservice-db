47fe372e5e2aa955f012ef5388bee210fcb2500f
syc <syc@partdb.com>
2025-04-17 23:38:43 +0900

    feature: 테스트 - 데이터 롤백

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -1,17 +1,20 @@
 package hello.itemservice.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @SpringBootTest
 class ItemRepositoryTest {
@@ -19,12 +22,26 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
     }
 
     @Test
